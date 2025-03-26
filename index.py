d93b1c741c8432ae412d25ddf0ea32d08b8f4912
bibia15 <163008110+bibia15@users.noreply.github.com>
2025-03-26 09:20:57 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,19 @@
 <!DOCTYPE python>
 <python lang="pt-Br">
-<head>
+import random
+palavra secreta= "gato"
+letras acertadas=["g", "a", "t", "o"]
+tentativas= 2
 
+while tentativas> o and " " in letras acertadas:
+palpite= input("digite uma letra") lower()
+
+ or palpite == letra:letras acertadas[index]= letra index +=1
+else:
+tentativa= 1
+print ("você tem {1 tentativa} restante")
+print(" " for (letras_acertadas))
+if" " not  in letras acertadas:
+print("parabéns você ganhou!")
+else 
+print("que pena,você perdeu: a palavra era:", palavra_secreta)
